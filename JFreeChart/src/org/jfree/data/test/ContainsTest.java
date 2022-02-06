d15fe0d2a34e70d37a187c7e93c1bb221931eb6b
Taaha Shakeel <81483265+tshakeel-debug@users.noreply.github.com>
2022-02-06 16:05:19 -0700

    Update ContainsTest.java

--- JFreeChart/src/org/jfree/data/test/ContainsTest.java
+++ JFreeChart/src/org/jfree/data/test/ContainsTest.java
@@ -7,37 +7,40 @@ import org.jfree.data.Range;
 
 public class ContainsTest {
 private Range testR;
+
+/* The range of 10.0-30.0 is created for testing */
 @Before
 public void setUp() throws Exception
 	{
 	testR = new Range(10.0, 30.0);
 	}
+/* Testing to see if range contains a number in between the upper and the lower bound */
 @Test
 public void testValueInRangeForMethodContains() {
 	
 
 	assertEquals("The value lies in Range.", true, testR.contains(20.0));
 }
-
+/* Testing to see if range contains a number that is less than the lower bound */
 @Test
 public void testValueLessThanLowerBoundForMethodContains() {
 
 	assertEquals("The value is less than lower bound range value and lies inside Range.", false, testR.contains(-10.0));
 }
-
+/* Testing to see if range contains a number that is higher than the upper bound */
 @Test
 public void testValueMoreThanUpperBoundForMethodContains() {
 
 	assertEquals("The value is more than upper bound range value and lies inside Range.", false, testR.contains(40.0));
 }
 
-
+/* Testing to see if range contains the lower bound */
 @Test
 public void testLowerBoundRangeValueForMethodContains() {
 
 	assertEquals("The lower bound value lies in Range.", true, testR.contains(10.0));
 }
-
+/* Testing to see if range contains the upper bound */
 @Test
 public void testUpperBoundRangeValueForMethodContains() {
 
