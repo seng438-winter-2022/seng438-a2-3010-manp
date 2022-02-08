441a1566df04ea86c2c70f98388fa509c684ffb1
Taaha Shakeel <81483265+tshakeel-debug@users.noreply.github.com>
2022-02-08 13:07:27 -0700

    Update GetLowerBoundTest.java

--- JFreeChart/src/org/jfree/data/test/GetLowerBoundTest.java
+++ JFreeChart/src/org/jfree/data/test/GetLowerBoundTest.java
@@ -8,18 +8,21 @@ import org.jfree.data.Range;
 public class GetLowerBoundTest {
 private Range testR;
 private Range testR2;
+/* Two Ranges are defined below (10.0-30.0 and -21.3--20.0) for testing purpose */
 @Before
 public void setUp() throws Exception
 	{
 	testR = new Range(10.0, 30.0);
 	testR2 = new Range(-21.3, -20.0);
 	}
+/* Testing to see if correct positive lower boubd value is returned  */
 @Test
 public void testLowerBoundPositiveValueForMethodGetLowerBound() {
 	
 	assertEquals("The Lower bound value is.", 10.0, testR.getLowerBound(), .0000001d);
 }
-
+	
+/* Testing to see if correct negative lower bound value is returned  */
 @Test
 public void testLowerBoundNegativeValueForMethodGetLowerBound() {
 	
