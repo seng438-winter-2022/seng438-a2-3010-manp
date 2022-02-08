70782b160b89cdebe4bd5c881886743b801420e3
tiz00013 <tianfan.zhou@ucalgary.ca>
2022-02-07 17:11:35 -0700

    update

--- JFreeChart/src/org/jfree/data/test/ToStringTest.java
+++ JFreeChart/src/org/jfree/data/test/ToStringTest.java
@@ -9,12 +9,15 @@ public class ToStringTest {
 	private Range test1;
 	private Range test2;
 	private Range test3;
+	private Range test4;
+	private Range test5;
 	
     @Before
     public void setUp() throws Exception {
     	test1 = new Range (10,30);
     	test2 = new Range (-30, -10);
     	test3 = new Range (10.55, 99.55);
+    	test4 = new Range (0,0);
     }
 
     @Test
@@ -33,6 +36,23 @@ public class ToStringTest {
     public void testTwoDecimalForMethodToString() {
     	assertEquals("Range with lower 10.5 and upper 99.5, by calling toString method",
     			"Range[10.55,99.55]", test3.toString());
+    }
+    
+    @Test
+    public void testSameBoundForMethodToString() {
+    	assertEquals("Range with lower 0 and upper 0,by calling toString method",
+    			"Range[0.0,0.0]", test4.toString());
+    }
+    
+    @Test
+    public void testNullForMethodToString() {
+    	boolean flag = false;
+    	try {
+    		test5.toString();
+    	}catch(Exception e) {
+    		flag = true;
+    	}
+    	assertTrue("Test with uninitialized input",flag);
     }
     
     @After
